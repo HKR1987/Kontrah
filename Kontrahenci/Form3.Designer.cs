3fb19e1681e26ecc25b1b24540099d4103b426ba
HKR <hkr1987@gmail.com>
2018-04-27 15:41:20 +0200

    dod1

--- Kontrahenci/Form3.Designer.cs
+++ Kontrahenci/Form3.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(325, 40);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
--- Kontrahenci/Form3.cs
+++ Kontrahenci/Form3.cs
@@ -16,31 +16,24 @@ namespace Kontrahenci
         {
             InitializeComponent();
             TextBox[] textboxy = new TextBox[25];
-            string[] nazwy ={ "b_nazwa", "b_nazwa_skrocona", "b_adr_ulica", "b_adr_nr_dom", "b_adr_nr_mieskzania", "b_adr_kod_pocztowy", "b_adr_miejscowosc", "b_adrk_ulica", "b_adrk_nr_dom", "b_adrk_nr_mieskzania", "b_adrk_kod_pocztowy", "b_adrk_miejscowosc", "b_telefon", "b_email", "b_aktywny", "b_nip", "b_pesel", "b_dow_seria", "b_dow_numer", "b_dow_wydal", "b_dow_data_wyd", "b_bank_nazwa", "b_bank_numer", "b_zapl_forma", "b_zapl_termin"};
+            string[] nazwy = { "b_nazwa", "b_nazwa_skrocona", "b_adr_ulica", "b_adr_nr_dom", "b_adr_nr_mieskzania", "b_adr_kod_pocztowy", "b_adr_miejscowosc", "b_adrk_ulica", "b_adrk_nr_dom", "b_adrk_nr_mieskzania", "b_adrk_kod_pocztowy", "b_adrk_miejscowosc", "b_telefon", "b_email", "b_aktywny", "b_nip", "b_pesel", "b_dow_seria", "b_dow_numer", "b_dow_wydal", "b_dow_data_wyd", "b_bank_nazwa", "b_bank_numer", "b_zapl_forma", "b_zapl_termin" };
             for (int i = 0; i < textboxy.Length; i++)
             {
+                //MessageBox.Show(nazwy[i]);
                 var txt = new TextBox();
                 textboxy[i] = txt;
                 txt.Name = nazwy[i];
                 txt.Text = nazwy[i];
-                txt.Location = new Point(5, 32 + (i * 28));
+                txt.Location = new Point(12, 1 + (i * 28));
                 txt.Visible = true;
+                this.Controls.Add(textboxy[i]);
             }
         }
 
         private void b_zapisz_Click(object sender, EventArgs e)
         {
-            TextBox[] textboxy = new TextBox[25];
-            string[] nazwy = { "b_nazwa", "b_nazwa_skrocona", "b_adr_ulica", "b_adr_nr_dom", "b_adr_nr_mieskzania", "b_adr_kod_pocztowy", "b_adr_miejscowosc", "b_adrk_ulica", "b_adrk_nr_dom", "b_adrk_nr_mieskzania", "b_adrk_kod_pocztowy", "b_adrk_miejscowosc", "b_telefon", "b_email", "b_aktywny", "b_nip", "b_pesel", "b_dow_seria", "b_dow_numer", "b_dow_wydal", "b_dow_data_wyd", "b_bank_nazwa", "b_bank_numer", "b_zapl_forma", "b_zapl_termin" };
-            for (int i = 0; i < textboxy.Length; i++)
-            {
-                var txt = new TextBox();
-                textboxy[i] = txt;
-                txt.Name = nazwy[i];
-                txt.Text = nazwy[i];
-                txt.Location = new Point(5, 32 + (i * 28));
-                txt.Visible = true;
-            }
+            
+            
 
         }
     }
